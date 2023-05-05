e0f24a8f1a619d668ac829b2cf906c340be26aa4
Erin Kennedy <frankenteddy@Erins-MacBook-Pro.local>
2023-05-05 08:50:52 -0600

    Integrated code start

---
+++ code/AtmosphinderMDRS/Board.h
@@ -0,0 +1,12 @@
+#define BATT_LEVEL_PIN    A17
+#define HEARTBEAT_LED     36
+#define CURRENT_SENSOR    A16
+#define AUX_LED           A14
+#define CENTRAL_LED       30
+#define LED               13
+#define WIND_SENSOR       A9
+#define BUTTON_PIN        26
+#define SERVO1_PIN        4
+#define SERVO2_PIN        5
+#define SERVO3_PIN        6
+#define SERVO4_PIN        9
